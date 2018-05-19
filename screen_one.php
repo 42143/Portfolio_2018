b1fdb0ae847bdabbb6650b9ab9e5075059deaea5
42143 <aslan-k@hotmail.com>
2018-05-19 01:19:59 -0300

    atualização do portfolio

--- screen_one.php
+++ screen_one.php
@@ -1,11 +1,33 @@
 <div class="container-fluid Screen screen1" >
     <div class="row">
-        <div class="col intro">
-            <h3><span>{{</span>Olá!, meu nome é Aslan kelvin<span>}}</span></h3>
-            <!--<h3>MEU NOME É ASLAN KELVIN</h3>-->
-            <h1>SOU WEBMASTER<br>FRONT-END & BACK-END</h1>
-            <i class="fab fa-connectdevelop fa-5x "></i>
+        <div class="col-5">
+            <!--<h3><span>{{</span>Olá!, meu nome é Aslan kelvin<span>}}</span></h3>
+            <!--<h3>MEU NOME É ASLAN KELVIN</h3>
+            <h1>SOU WEBMASTER<br>FRONT-END & BACK-END</h1>-->
         </div>
+        <div class="col wrapper">
+           <svg x="0px" y="0px" viewBox="0 0 388 338.6" style="enable-background:new 0 0 388 338.6;" xml:space="preserve">
+            <style type="text/css">
+                .st3{fill:none;stroke:#F06;stroke-miterlimit:10;}
+                .st4{fill:#F06;}
+            </style>
+            <polygon class="st3" points="15.1,172 103.6,15.2 289.5,15.2 379.2,172 288.2,331.9 103.6,331.9" stroke-width="9"/>
+            <circle class="st4" cx="195.9" cy="13.8" r="8.8"/>
+            <circle class="st4" cx="195.9" cy="329.8" r="8.8"/>
+            <circle class="st4" cx="105.9" cy="329.8" r="8.8"/>
+            <circle class="st4" cx="15.1" cy="172" r="8.8"/>
+            <circle class="st4" cx="56.2" cy="250" r="8.8"/>
+            <circle class="st4" cx="379.2" cy="172" r="8.8"/>
+            <circle class="st4" cx="291.2" cy="329.8" r="8.8"/>
+            <circle class="st4" cx="333.2" cy="253.8" r="8.8"/>
+            <circle class="st4" cx="291.2" cy="15.2" r="8.8"/>
+            <circle class="st4" cx="105.9" cy="13.8" r="8.8"/>
+            <circle class="st4" cx="333.2" cy="92.8" r="8.8"/>
+            <circle class="st4" cx="61" cy="92.8" r="8.8"/>
+            <polygon class="st3" points="59.8,92.8 289.4,329.8 333.2,92.8 105.9,329.8 105.9,13.8 333.2,253.8 56.2,250 289.5,15.2 " stroke-width="5"/>
+            </svg>
+        </div>
+        <div class="col-5"></div>
         <div class="w-100"></div>
         <div class="col cashier">
             <div class="code">
